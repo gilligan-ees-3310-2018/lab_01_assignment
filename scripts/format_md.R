3384111b60c69fa915aca7661bb9039a8be24e11
Jonathan Gilligan <jonathan.gilligan@gmail.com>
2018-08-16 20:54:46 -0500

    Updated Lab #1 assignment.

--- scripts/format_md.R
+++ scripts/format_md.R
@@ -1,23 +1,71 @@
 library(tidyverse)
 library(stringr)
 
-format_md = function(x, digits = NULL, scientific = NULL, comma = FALSE) {
-  if (! is.null(digits)) x = signif(x, digits + 1)
-  if (! is.null(scientific)) {
-    if(str_to_lower(scientific) == 'auto') scientific = NULL
+format_engr = function(x, digits = 6) {
+  x_sci <- formatC(x, digits = digits, format = "e", flag = "#",
+                   drop0trailing = FALSE)
+  parts <- str_match(x_sci,
+                     str_c("^",
+                           "(<sign>[+-])?",
+                           "(?<int>[0-9]+)",
+                           "(?<decimal>\\.(?<frac>[0-9]+)?)?",
+                           "([Ee](?<exp>(?<expsign>[+-])?(?<expval>[0-9]+)))?"
+                           )) %>%
+    set_names(c("x_sci", "sign", "int", "decimal", "frac",
+                "exp_str", "exp", "exp_sign", "exp_val"))
+
+  if (! is.na(parts['exp'])) {
+    exp <- as.integer(parts['exp'])
+
+    exp_adj <- exp %% 3
+    if (exp_adj > 0) {
+      if (is.na(parts['frac'])) {
+        parts['frac'] <- ""
+      }
+      if (str_length(parts['frac']) < exp_adj) {
+        parts['frac'] <- str_c(parts['frac'],
+                            strrep("0", exp_adj - str_length(parts['frac'])))
+      }
+      delta <- str_sub(parts['frac'], end = exp_adj)
+      parts['frac'] <- str_sub(parts['frac'], exp_adj + 1)
+      parts['int'] <- str_c(parts['int'], delta)
+      parts['exp'] <- as.character( as.integer(parts['exp']) - exp_adj )
+      parts <- parts %>% map_chr(~ifelse(is.na(.x), "", .x))
+      x_sci <- str_c(parts['sign'], parts['int'], ".", parts['frac'],
+                     "e", parts['exp'])
+    }
   }
-  if (is.null(scientific)) {
-    fixed = formatC(x, digits = digits, format = 'fg', flag = '#')
-    sci = formatC(x, digits = digits, format = 'e')
-    format = ifelse(str_length(fixed) > getOption('scipen') + str_length(sci), 'e', 'fg')
-  }
-  else {
-    format = ifelse(scientific, 'e', 'fg')
-  }
+  x_sci
+}
+
+format_md = function(x, digits = NULL,
+                     format = c('normal', 'auto', 'scientific',
+                                'engineering'),
+                     comma = FALSE) {
+  format = match.arg(format)
   mark = ifelse(comma, ',', '')
 
-  formatC(x, digits = digits, format = format,
-          flag = '#',
-          big.mark = mark) %>%
-    str_replace_all(c('\\+' = '', '[Ee](-?)0*([1-9][0-9]*)$' = '&times;10^\\1\\2^'))
+  fixup_scientific = function(s) {
+    str_replace_all(s,  c('\\+' = '',
+                          '[Ee](-?)0*([1-9][0-9]*)$' = '&times;10^\\1\\2^'))
+  }
+
+  if (! is.null(digits)) x = signif(x, digits + 1)
+  if (format == 'auto') {
+    fixed = formatC(x, digits = digits, format = 'fg', flag = '#',
+                    big.mark = mark)
+    sci = formatC(x, digits = digits, format = 'e')
+    formatted = ifelse(str_length(fixed) > getOption('scipen') + str_length(sci),
+                      fixup_scientific(sci), fixed)
+  } else if (format == "normal") {
+    formatted = formatC(x, digits = digits, format = 'fg', flag = '#',
+                        big.mark = mark)
+  } else if (format == "scientific") {
+    formatted = formatC(x, digits = digits, format = 'e') %>%
+      fixup_scientific()
+  } else if (format == "engineering") {
+    formatted = format_engr(x, digits = digits) %>%
+      fixup_scientific()
+  }
+  formatted
 }
